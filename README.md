9207d41a961d236053c4f3ac486d48c3ce0c0dc2
John Zechlin <johnzech@gmail.com>
2021-05-08 23:28:17 -0600

    Add knife.py, a fork utility.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # knife
 Knife
+
+## Prereqs
+* Python 3.x
+* Flask `pip3 install flask` (on many platforms) 
+* Requests `pip3 install requests` (on many platforms)
+
+## References
+* https://docs.github.com/en/developers/apps/authorizing-oauth-apps
+* https://github.com/settings/applications/new
+* https://docs.github.com/en/developers/apps/scopes-for-oauth-apps#available-scopes
+* https://docs.github.com/en/rest/reference/repos#forks
+
+## To set up and run locally
+* Install prereqs
+* run python3 knife.py
+
+
---
+++ knife.py
@@ -0,0 +1,83 @@
+
+import json
+import requests
+from flask import Flask, render_template, request
+
+conf = {
+    'github_client_id': 'cd88fcd54ec2d4020d03',
+    'github_client_secret': 'e01a66ac4a63fe81f1e4ca3f7787560d0e3a9d7b',
+    'github_authorize_url': 'https://github.com/login/oauth/authorize',
+    'self_repo_url': 'https://github.com/ForksInc/knife',
+}
+
+application = Flask(__name__)
+state = "Z70tYH9R584SCsuOBqGFC9FBafRICc3YZAWE"
+
+@application.route("/")
+def hello():
+    return '''
+    <h1 style='color:blue'>Want to fork the knife repo?</h1>
+    <p><a href="{url}?client_id={client_id}&state={state}&scope=repo">Absolutely!</a></p>
+    '''.format(url=conf['github_authorize_url'], client_id=conf['github_client_id'], state=state)
+
+@application.route("/authenticated/")
+def authenticated():
+    try:
+        code = request.args['code']
+        responded_state = request.args['state']
+        if (responded_state != state):
+            raise Exception
+    except:
+        return '''
+            <h1>Not Authenticated</h1>
+            <p>Oh dear. You've landed on this page with missing or invalid auth data.</p>
+            <p>You'll probably want to <a href="/">start over.</a>
+        '''
+    else:
+        success, token = get_token(code)
+        if success:        
+            success, new_repo_name = fork_repo(token)
+            
+            if success:
+                url = "https://github.com/{path}".format(path=new_repo_name)
+                return '''
+                <h1 style='color:green'>Ok, cool. It's done.</h1>
+                <p>Your fork... <a href="{url}">{url}</a></p>
+                '''.format(url=url)
+
+    # fail over to this catch all error output    
+    return '''
+    <h1 style='color:red'>Oh no.</h1>
+    <p>Unfortunately, something went wrong. You can try a few things...</p>
+    <ul>
+        <li><a href="/">Start over</a></li>
+        <li>Use the github native feature: <a href="{self_url}/fork">{self_url}/fork</a></li>
+    </ul>
+    '''.format(self_url=conf['self_repo_url'])       
+
+def get_token(code):
+    url = "https://github.com/login/oauth/access_token"
+    data = {
+        'client_id': conf['github_client_id'],
+        'client_secret': conf['github_client_secret'],
+        'code': code,
+        'state': "jkhkjh"
+    }
+    headers = {'Accept': 'application/json'}
+    response = requests.post(url, data=data, headers=headers)
+    try:
+        return True, json.loads(response.text)['access_token']
+    except:
+        return False, ''
+
+def fork_repo(token):
+    url = "https://api.github.com/repos/ForksInc/knife/forks"
+    headers = {"Authorization": "token %s" % token}
+    response = requests.post(url, headers=headers)
+    if (response.status_code == 202):
+        return True, json.loads(response.text)['full_name']
+    else:
+        return False, response.status_code
+
+if __name__ == "__main__":
+    application.run()
